c665695051c3a73fd16a75a9ff7a2eb7a2b7420c
esc122 <41628470+esc122@users.noreply.github.com>
2022-08-20 10:42:12 +0900

    Add files via upload

--- joy_sub_raspi_move.py
+++ joy_sub_raspi_move.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Joy
-
 import pigpio
-from time import sleep
 
 pi = pigpio.pi()
-pi.set_mode(25, pigpio.OUTPUT) 
-pi.set_mode(24, pigpio.OUTPUT) 
-pi.set_mode(23, pigpio.OUTPUT) 
 pi.set_mode(22, pigpio.OUTPUT) 
-pi.set_PWM_frequency(25, 100)
-pi.set_PWM_frequency(24, 100)
-pi.set_PWM_frequency(23, 100)
+pi.set_mode(23, pigpio.OUTPUT) 
+pi.set_mode(24, pigpio.OUTPUT) 
+pi.set_mode(25, pigpio.OUTPUT) 
 pi.set_PWM_frequency(22, 100)
+pi.set_PWM_frequency(23, 100)
+pi.set_PWM_frequency(24, 100)
+pi.set_PWM_frequency(25, 100)
+
 duty_forward = 150
 duty_direction = duty_forward + 5
 duty_max =255
@@ -27,31 +26,35 @@ def callback(joy_msg):
     if axes == [0, 1]:
         print('forward')
         duty = duty_forward
-        pi.set_PWM_dutycycle(25, 0)
-        pi.set_PWM_dutycycle(24, duty)
-        pi.set_PWM_dutycycle(23, 0)
         pi.set_PWM_dutycycle(22, duty)
+        pi.set_PWM_dutycycle(23, 0)
+        pi.set_PWM_dutycycle(24, duty)
+        pi.set_PWM_dutycycle(25, 0)
+
     elif axes == [0, -1]:
         print('back')
         duty = duty_forward
-        pi.set_PWM_dutycycle(25, duty)
-        pi.set_PWM_dutycycle(24, 0)
-        pi.set_PWM_dutycycle(23, duty)
         pi.set_PWM_dutycycle(22, 0)
+        pi.set_PWM_dutycycle(23, duty)
+        pi.set_PWM_dutycycle(24, 0)
+        pi.set_PWM_dutycycle(25, duty)
+
     elif axes == [-1, 0]:
         print('right')
         duty = duty_direction
-        pi.set_PWM_dutycycle(25, 0)
-        pi.set_PWM_dutycycle(24, duty)
-        pi.set_PWM_dutycycle(23, 0)
         pi.set_PWM_dutycycle(22, 0)
-    elif axes == [1, 0]:
-        print('lest')
-        duty = duty_direction
-        pi.set_PWM_dutycycle(25, 0)
-        pi.set_PWM_dutycycle(24, 0)
         pi.set_PWM_dutycycle(23, 0)
+        pi.set_PWM_dutycycle(24, duty)
+        pi.set_PWM_dutycycle(25, 0)
+
+    elif axes == [1, 0]:
+        print('left')
+        duty = duty_direction
         pi.set_PWM_dutycycle(22, duty)
+        pi.set_PWM_dutycycle(23, 0)
+        pi.set_PWM_dutycycle(24, 0)
+        pi.set_PWM_dutycycle(25, 0)
+
     elif buttonsA == 1:
         if duty_forward <= duty_max - 10:
             duty_forward = duty_forward + 10
@@ -62,12 +65,13 @@ def callback(joy_msg):
             duty_forward = duty_forward - 10
             duty_direction = duty_forward + 5
         print('duty : ', duty_forward)
+
     else:
         print('stop')
-        pi.set_PWM_dutycycle(25, 0)
-        pi.set_PWM_dutycycle(24, 0)
-        pi.set_PWM_dutycycle(23, 0)
         pi.set_PWM_dutycycle(22, 0)
+        pi.set_PWM_dutycycle(23, 0)
+        pi.set_PWM_dutycycle(24, 0)
+        pi.set_PWM_dutycycle(25, 0)
 
 if __name__ == '__main__':
     rospy.init_node('joy_sub_raspi_move', anonymous=True)
